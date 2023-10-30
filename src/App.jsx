d8bfbdc405737649fbc70ee67771c42387e362fe
waiyanhtun282 <waiyanhtun282@gmail.com>
2023-10-30 15:04:57 +0630

    set up

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+const App = () => {
+  return (
+    <div>
+      <Button variant="" color="error">Text</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "5rem",
+      },
+    },
+  },
+  plugins: [],
+});
